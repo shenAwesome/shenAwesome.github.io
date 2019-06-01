a42f220254ca027ad2994a43f1649e90cfb2a24a
Shen Z <shenAWesome@gmail.com>
2019-06-01 20:24:32 +1000

    thumbnails

--- show/rename.py
+++ show/rename.py
@@ -1,12 +1,18 @@
 import fnmatch
 import os
+from PIL import Image
 
-dir = r'C:\Shen\work\screenshots\next\origin'
+dir = r'C:\Shen\work\screenshots\next'
 
 matches = []
 for root, dirnames, filenames in os.walk(dir):
-    for filename in fnmatch.filter(filenames, '*.jpg'):
+    for filename in fnmatch.filter(filenames, '*.png'):
         matches.append(os.path.join(root, filename))
         
 for f in matches:
-    os.rename(f,f.replace('.jpg','.png'))
+    if not '_small' in f:
+        im = Image.open(f)
+        #im = im.convert("RGB")
+        im = im.resize((200,200))
+        im.save(f.replace('.png','_small.png'))
+        #os.rename(f,f.replace('.jpg','.png'))
